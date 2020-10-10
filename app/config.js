07e58f7229039a68bddca7512f088a29ec4a6e31
Sijia Meng <sm7515@nyu.edu>
2020-10-10 14:22:02 -0400

    change api url

--- app/config.js
+++ app/config.js
@@ -1,2 +1,2 @@
-export const apiUrl = 'http://localhost:3000/api';
-export const baseUrl = 'http://localhost:3000/app';
+export const apiUrl = 'https://gpa-4-0.herokuapp.com/api';
+export const baseUrl = 'https://gpa-4-0.herokuapp.com/app';
