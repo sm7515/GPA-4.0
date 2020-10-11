302be7b2017daf5b250dc0891e7859d206c6749e
Sijia Meng <sm7515@nyu.edu>
2020-10-10 21:00:24 -0400

    minor

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 export const apiUrl = 'https://gpa-4-0.herokuapp.com/api';
-export const baseUrl = 'https://gpa-4-0.herokuapp.com/app';
-// export const baseUrl = 'http://localhost:3000/app';
+// export const baseUrl = 'https://gpa-4-0.herokuapp.com/app';
+export const baseUrl = 'http://localhost:3000/app';
 // export const apiUrl = 'http://localhost:3000/api';
